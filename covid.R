c45cd2622b7b7af62c892d072516d18c95aac958
Bruno Zamorano <bzamorano@MacBookAir.local>
2020-05-27 11:09:15 +0200

    Fix to account for new data format

--- covid.R
+++ covid.R
@@ -20,6 +20,7 @@ data <- read.csv2(tf, sep=",", header = TRUE)
 
 # Fix the data first
 data[[1]] <- as.POSIXct(strptime(data[[1]], "%d/%m/%Y"))
+data <- data %>% arrange(desc(dateRep))
 
 nrows <- nrow(data)
 
@@ -32,15 +33,17 @@ countries <- unique(data$countriesAndTerritories)
 
 for (country in countries) {
   irows <- (1:nrows)[data$countriesAndTerritories == country]
-  n_rows <- length(irows)
+  dt <- data[irows,]
+
+  n_rows <- nrow(dt)
   
-  for (i in irows ) {
-    min_index <- min(i+7, irows[n_rows])
+  for (i in 1:n_rows ) {
+    min_index <- min(i+7, n_rows)
     
-    weeknew[i] <- sum(data$cases[i:min_index])
-    tot_sum[i] <- sum(data$cases[i:irows[n_rows]])
-    weekdeath[i] <- sum(data$deaths[i:min_index])
-    death_sum[i] <- sum(data$deaths[i:irows[n_rows]])
+    weeknew[irows[i]] <- sum(dt$cases[i:min_index])
+    tot_sum[irows[i]] <- sum(dt$cases[i:n_rows])
+    weekdeath[irows[i]] <- sum(dt$deaths[i:min_index])
+    death_sum[irows[i]] <- sum(dt$deaths[i:n_rows])
   }
 }
 
@@ -66,7 +69,7 @@ data%>%
 data %>%
   group_by(countriesAndTerritories) %>%
   filter(max(popData2018) > 5000) %>%
-  filter(sum(deaths) > 20) %>%
+  filter(sum(deaths) > 100) %>%
   summarise(Deaths = sum(deaths),
             Population = max(popData2018),
             Death_rate = sum(deaths) * 1.e6 / max(popData2018) ) %>%
@@ -75,15 +78,15 @@ data %>%
   ggplot(aes(x = reorder(countriesAndTerritories, -Death_rate), weight = Death_rate)) +
   geom_bar(fill = "red", colour = "darkred") +
   coord_flip() +
-  annotate("text", x=14.8, y=1000, label= "Countries with pop. > 5000") +
-  annotate("text", x=14, y=1000, label= "and at least 20 deaths") +
+  annotate("text", x=14.8, y=625, label= "Countries with pop. > 5000") +
+  annotate("text", x=14, y=625, label= "and at least 100 deaths") +
   labs(x = "Country", y = "Deaths per million inhabitants")
 
 # Bottom death rate
 data %>%
   group_by(countriesAndTerritories) %>%
   filter(max(popData2018) > 5000) %>%
-  filter(sum(deaths) > 20) %>%
+  filter(sum(deaths) > 100) %>%
   summarise(Deaths = sum(deaths),
             Population = max(popData2018),
             Death_rate = sum(deaths) * 1.e6 / max(popData2018) ) %>%
@@ -92,8 +95,8 @@ data %>%
   ggplot(aes(x = reorder(countriesAndTerritories, -Death_rate), weight = Death_rate)) +
   geom_bar(fill = "blue", colour = "darkblue") +
   coord_flip() +
-  annotate("text", x=14.8, y=2, label= "Countries with pop. > 5000") +
-  annotate("text", x=14, y=2, label= "and at least 20 deaths") +
+  annotate("text", x=14.8, y=5.5, label= "Countries with pop. > 5000") +
+  annotate("text", x=14, y=5.5, label= "and at least 100 deaths") +
   labs(x = "Country", y = "Deaths per million inhabitants")
 
 # By continent
@@ -105,13 +108,29 @@ data %>%
   group_by(Continent) %>%
   summarise(Deaths = sum(Deaths, na.rm = TRUE),
             Population = sum(Population, na.rm = TRUE)) %>%
-  ggplot(aes(x = reorder(Continent, -Population), y = Deaths)) +
-  geom_point(aes(size = Population), color = "darkred") +
+  filter(Population > 5000) %>%
+  ggplot(aes(x = Population, y = Deaths)) +
+  geom_point(aes(color = Continent), size = 7 ) +
+  labs(x = "Population", y = "Deaths")
+
+# By continent (log-scale)
+data %>%
+  group_by(countriesAndTerritories) %>%
+  summarise(Deaths = sum(deaths),
+            Population = max(popData2018),
+            Continent = first(continentExp)) %>%
+  group_by(Continent) %>%
+  summarise(Deaths = sum(Deaths, na.rm = TRUE),
+            Population = sum(Population, na.rm = TRUE)) %>%
+  filter(Population > 5000) %>%
+  ggplot(aes(x = Population, y = Deaths)) +
+  geom_point(aes(color = Continent), size = 7 ) +
   scale_y_log10(labels = comma_format(big.mark = " ")) +
-  labs(x = "Continent", y = "Deaths")
+  scale_x_log10(labels = comma_format(big.mark = " ")) +
+  labs(x = "Population", y = "Deaths")
 
 # This can be changed to account for more countries
-countries <- unique(data$countriesAndTerritories[data$totDeath > 5000 
+countries <- unique(data$countriesAndTerritories[data$totDeath > 10000 
                                     | data$countriesAndTerritories == "South_Korea"])
 small <- data[data$countriesAndTerritories %in% countries,]
 
@@ -150,9 +169,7 @@ small[(small$totDeath > 100), ]%>%
   scale_y_log10(labels = comma_format(big.mark = " ")) +
   labs(x = "Total deaths", y = "Weekly deaths", colour = "Country")
 
-
 # But the real deal is this:
-
 small[(small$totCases > 100), ]%>%
   ggplot(aes(x=totCases, y=weekNew, colour = countriesAndTerritories)) +
   geom_line(size=1) +
