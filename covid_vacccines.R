763e0f6aa91bf073b5a2d61d017eeae0018d062b
Bruno Zamorano <bzamorano@ugr.es>
2021-06-15 13:26:00 +0200

    Latest models and data

--- covid_vacccines.R
+++ covid_vacccines.R
@@ -46,11 +46,14 @@ get_date <- function(country){
     f <- fitModel(people_fully_vaccinated_per_hundred ~ (A*Days+B)^E+C*sin( (Days-D)/7 ), data = x, 
                   start=list(A=4e-4, B=1, C=0.06, D=33, E =54))
   }else if(country == "Israel"){
-    f <- fitModel(people_fully_vaccinated_per_hundred ~ A + B*Days^C, data = x,
-                  start=list(A=-10, B=2, C=0.8))
+#    f <- fitModel(people_fully_vaccinated_per_hundred ~ A + B*Days^C, data = x,
+#                  start=list(A=-90, B=60, C=0.18))
+    # testing it for now
+    f <- fitModel(people_fully_vaccinated_per_hundred ~ A*log(B*(Days+C)), data = x, 
+                  start=list(A=30, B=0.05, C=13))
   }else if(country == "Japan"){
     f <- fitModel(people_fully_vaccinated_per_hundred ~ A + B*Days^C, data = x,
-                  start=list(A=-0.01, B=4e-12, C=6))
+                  start=list(A=0.06, B=8e-14, C=6))
   }else if(country == "Mexico" | country == "Belgium" ){
     f <- fitModel(people_fully_vaccinated_per_hundred ~ A + B*Days^C, data = x,
                   start=list(A=-0.1, B=2.7e-8, C=4))
