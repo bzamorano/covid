b6ea33c4228b2d2bf4cfefeb441319626e912d4c
Bruno Zamorano <bzamorano@ugr.es>
2021-10-05 15:46:08 +0200

    Latest data and report. Minor fix to models

--- covid_vacccines.R
+++ covid_vacccines.R
@@ -40,7 +40,7 @@ get_date <- function(country){
   
   f2 <- linearModel(formula = people_fully_vaccinated_per_hundred ~ Days + 1, data = x2)
 
-  if(country == "Belgium" | country == "Canada")
+  if(country == "Canada")
     {
     # En realidad es el modelo más genérico
     f <- fitModel(people_fully_vaccinated_per_hundred ~ A + B*Days^C, data = x,
@@ -53,7 +53,7 @@ get_date <- function(country){
                   start=list(A=-0.1, B=2.7e-8, C=4))
   }else if(country == "Sweden" | country == "Portugal" | country == "Spain"
            | country == "Mexico" | country == "Brazil" | country == "France"
-           | country == "Italy" | country == "Germany"){
+           | country == "Italy" | country == "Germany" | country == "Belgium"){
     f <- fitModel(people_fully_vaccinated_per_hundred ~ A + B*Days^C, data = x,
                   start=list(A=-0.3, B=2.7e-5, C=2.6))
   }else{
