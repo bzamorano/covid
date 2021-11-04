9c5f273d9e80304d803f5f253bd845dfcc887d65
Bruno Zamorano <bzamorano@ugr.es>
2021-11-04 10:35:19 +0100

    Minor fix. Latest data and report

--- covid_vacccines.R
+++ covid_vacccines.R
@@ -56,7 +56,7 @@ get_date <- function(country){
     f <- fitModel(people_fully_vaccinated_per_hundred ~ A + B*Days^C, data = x,
                   start=list(A=-0.1, B=2.7e-8, C=4))
   }else if(country == "Portugal" | country == "Mexico" | country == "Brazil"
-           | country == "Canada" | country == "Japan"){
+           | country == "Japan"){
     f <- fitModel(people_fully_vaccinated_per_hundred ~ A + B*Days^C, data = x,
                   start=list(A=-0.3, B=2.7e-5, C=2.6))
   }else{
