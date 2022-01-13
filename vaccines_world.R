ec435c1285879f5fc966528bccfe36f527b512da
Bruno Zamorano <bzamorano@ugr.es>
2022-01-13 14:40:41 +0100

    Minor aesthetic fixes, latest data and graphs

--- vaccines_world.R
+++ vaccines_world.R
@@ -218,7 +218,7 @@ dTime %>%
              x=as.Date(Date) ))  +
   geom_point(show.legend = FALSE) +
   geom_errorbarh(aes(xmin = as.Date(Date)-Error, xmax = as.Date(Date)+Error), show.legend = FALSE) +
-  geom_text(aes(x = as.Date(Date)+Error+50+3*nchar(Country), label=paste0(Country)), show.legend = FALSE ) +
+  geom_text(aes(x = as.Date(Date)+Error+150+5*nchar(Country), label=paste0(Country)), show.legend = FALSE ) +
   labs(x = "Date", y = "Country)") +
   theme(axis.line.y=element_blank(),
         axis.text.y=element_blank(),
@@ -226,10 +226,10 @@ dTime %>%
         axis.title.y=element_blank(),
         axis.ticks.y=element_blank()) +
   geom_vline(xintercept = as.Date(LastDay), col = "red", size = 1) +
-  annotate("text", x=as.Date("2023-07-01"), y=9, label= "Predicted date for 80% population fully vaccinated") +
-  annotate("text", x=as.Date("2023-07-01"), y=7.75, label= paste("Using data up to", LastDay)) +
-  annotate("text", x=as.Date("2023-07-01"), y=6.5, label= "Model by Bruno Zamorano") +
-  annotate("text", x=as.Date("2023-07-01"), y=5.25, label= "Data taken from https://ourworldindata.org") +
+  annotate("text", x=as.Date("2025-01-01"), y=9, label= "Predicted date for 80% population fully vaccinated") +
+  annotate("text", x=as.Date("2025-01-01"), y=7.75, label= paste("Using data up to", LastDay)) +
+  annotate("text", x=as.Date("2025-01-01"), y=6.5, label= "Model by Bruno Zamorano") +
+  annotate("text", x=as.Date("2025-01-01"), y=5.25, label= "Data taken from https://ourworldindata.org") +
 ggsave("xwing_countries.png")
 
 dTime2 %>%
